d4c666fae698b74403ba555077cc3779d462c785
Jessica Hawkins <jessicarhawkins08@outlook.com>
2015-11-12 10:31:33 -0600

    added install homebrew line to top of brew.sh

--- brew.sh
+++ brew.sh
@@ -8,6 +8,8 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 # Make sure we’re using the latest Homebrew.
 brew update
 
